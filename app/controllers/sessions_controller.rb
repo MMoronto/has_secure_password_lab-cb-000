c4f3df589718ef7131eecf95936cba3f25ec583a
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-07-12 15:12:51 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,5 +7,12 @@ class SessionsController < ApplicationController
     @user = User.find_by(name: params[:user][:name])
     return head(:forbidden) unless @user.authenticate(params[:user][:password])
     session[:user_id] = @user.id
+    redirect_to homepage_path
   end
+  
+  def destroy
+    session.delete :user_id
+    redirect_to login_path
+  end
+  
 end
