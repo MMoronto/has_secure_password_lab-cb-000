fa1a20dc89d7003aaa0942dabe5a1c044536e47b
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-07-12 15:18:55 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,15 +10,19 @@ class UsersController < ApplicationController
       @user.save
       session[:user_id] = @user.id
     redirect_to homepage_path
-    
-  else
-    redirect_to new_users_path
+      
+    else
+      redirect_to new_users_path
+    end
   end
+  
+  def homepage
+    @user = User.find_by(id: session[:user_id])
   end
  
   private
  
   def user_params
-    params.require(:user).permit(:username, :password, :password_confirmation)
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
